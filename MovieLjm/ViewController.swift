8950fcb3d2692838fea3b863e5366ea95bfabc7b
LeeJeongMin <wjdals4108@icloud.com>
2021-05-24 22:00:27 +0900

    Initial Commit

---
+++ MovieLjm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieLjm
+//
+//  Created by 이정민 on 2021/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
